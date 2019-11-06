22756f28c78b0e03d8408e8eb407c37e2c50e8e5
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-11-06 16:03:06 +0100

    doc: use only Python 3 ; don't depend on a particular ubuntu version

--- supaero2020/doc/install.sh
+++ supaero2020/doc/install.sh
@@ -1,8 +1,8 @@
-sudo apt install curl aptitude git adb
-sudo sh -c "echo 'deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub xenial robotpkg' >> /etc/apt/sources.list.d/robotpkg.list"
+sudo apt install curl git adb lsb-release
+sudo sh -c "echo 'deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub $(lsb_release -cs) robotpkg' >> /etc/apt/sources.list.d/robotpkg.list"
 curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | sudo apt-key add -
 sudo apt-get update
-sudo apt install robotpkg-py35-pinocchio robotpkg-py35-qt4-gepetto-viewer-corba ipython3 python3-pip freeglut3 python3-matplotlib python3-pil.imagetk
+sudo apt install robotpkg-py3\*-pinocchio robotpkg-py3\*-qt4-gepetto-viewer-corba ipython3 python3-pip freeglut3 python3-matplotlib python3-pil.imagetk
 
 
 echo '
@@ -12,4 +12,3 @@ export LD_LIBRARY_PATH=/opt/openrobots/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=/opt/openrobots/lib/python3.5/site-packages:$PYTHONPATH
 ' >> ~/.bashrc
 source ~/.bashrc
-
